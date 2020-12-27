b06a65b0d3fbe0d0a93a5222284cf350835b202e
put your eyes in the most overall , you would see all world <33368418+youngmanuit@users.noreply.github.com>
2020-12-27 22:27:42 +0700

    Change

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I'm Divergent, I'm come from Viet Nam. I have just graduated from UIT specialized in FIT. I have 2 EoW in Nodejs and ReactJs</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section classN ame={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
